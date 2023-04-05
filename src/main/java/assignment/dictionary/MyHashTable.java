da92defadb621f739e0193ff542fe51a3c0b5ad3
Bryan Tang <bryanktang@gmail.com>
2023-04-05 13:16:10 -0500

    Initial Commit

---
+++ src/main/java/assignment/dictionary/MyHashTable.java
@@ -0,0 +1,25 @@
+package assignment.dictionary;
+
+/*
+
+ */
+
+//
+import java.util.*;
+import java.io.*;
+import java.util.Dictionary;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.BiFunction;
+
+/**
+
+ */
+public class MyHashTable<K,V>
+//        extends Dictionary<K,V>
+//        implements Map<K,V>, Cloneable, java.io.Serializable {
+{
+
+}
+
+
