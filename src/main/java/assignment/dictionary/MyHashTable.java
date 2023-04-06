811cd968a34eb8c38a016e07549e59f8453b2927
Bryan Tang <bryanktang@gmail.com>
2023-04-06 15:17:26 -0500

    Added missing functions

--- src/main/java/assignment/dictionary/MyHashTable.java
+++ src/main/java/assignment/dictionary/MyHashTable.java
@@ -19,6 +19,23 @@ public class MyHashTable<K,V>
 //        extends Dictionary<K,V>
 //        implements Map<K,V>, Cloneable, java.io.Serializable {
 {
+    public V put(K key, V value) {}
+
+    public V remove(K key) {}
+
+    public V get(K key) {}
+
+    public boolean containsKey(Object key) {}
+
+    public Set<K> keySet() {}
+
+    public Collection<V> values() {}
+
+    public boolean isEmpty() {}
+
+    public int size() {}
+
+    public void clear() {}
 
 }
 
